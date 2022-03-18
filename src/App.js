5a91adf715ee178d1f520a99d6dc0bf966e8933c
vineet2411sharma <sharma24vineet@gmail.com>
2022-03-18 18:07:09 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Intro from "./Intro/Intro";
 import FeaturedProject from "./featuredProject/FeaturedProject";
 import AchievementSection from "./achievementSection/AchievementSection";
 import Footer from "./footer/Footer";
+import { COLORS } from "./Values/Colors";
+import TechStack from "./TechStack/TechStack";
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundColor: COLORS.primary1 }}>
       <CssBaseline />
       <Topbar />
       <Intro />
+      <TechStack />
       <FeaturedProject />
       <AchievementSection />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ src/TechStack/TechStack.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TechStackIcon from "./TechStackIcon";
+
+function TechStack() {
+  return (
+    <div>
+      <TechStackIcon />
+    </div>
+  );
+}
+
+export default TechStack;
