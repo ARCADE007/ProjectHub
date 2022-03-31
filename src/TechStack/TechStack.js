d6f0de36e156a2b667761310ae49ab11c161e56d
vineetsharma24 <sharma24vineet@gmail.com>
2022-03-31 12:50:06 +0530

    Tech stack Compleet

--- src/TechStack/TechStack.js
+++ src/TechStack/TechStack.js
@@ -1,9 +1,27 @@
 import React from "react";
 import TechStackIcon from "./TechStackIcon";
+import { COLORS } from "../Values/Colors";
 
 function TechStack() {
   return (
-    <div>
+    <div
+      style={{
+        margin: "40px",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "3vw",
+          color: COLORS.white,
+          borderBottom: "5px solid",
+          borderColor: COLORS.primary2,
+          width: "18vw",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        Tech Stack
+      </h1>
       <TechStackIcon />
     </div>
   );
